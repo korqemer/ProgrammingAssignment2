bba39a00810b21b742a98bf9b87ee6e12243feda
korqemer <nmiras99@gmail.com>
2020-12-22 16:13:28 +0600

    init commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix create R object that stores initial matrix and its inverse
+## it is done to save time while finding the inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(initMatrix = matrix()){
+    inv <- NULL
+    set <- function(y){
+        initMatrix <<- y
+        m <<- NULL
+    }
+    get <- function(){
+        initMatrix
+    }
+    setInv <- function(inverse){
+        inv <<- inverse
+    }
+    getInv <- function(){
+        inv
+    }
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve gets inverse of initial matrix, and if it doesn't exist, calculates and stores in x
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+    m <- x$getInv()
+    if (!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInv(m)
+    m
 }
